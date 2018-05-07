25e54ceb46c98c32efbcb8dd9068759b3eeec068
Yuhui Huang <mrhuangyuhui@gmail.com>
2018-05-07 16:33:13 +0800

    * 添加Demo

--- workspace/my-crazy-kotlin/ch08/src/sec6/CompanionTest.kt
+++ workspace/my-crazy-kotlin/ch08/src/sec6/CompanionTest.kt
@@ -21,9 +21,22 @@ class MyClass {
     }
 }
 
+// 为伴生对象扩展方法
+fun MyClass.Companion.test() {
+    println("dog")
+}
+
+// 扩展伴生对象的属性
+val MyClass.Companion.pet
+    get() = "cat"
+
 fun main(args: Array<String>) {
     MyClass.output("banana")
     println(MyClass.name)
     // 伴生对象省略名字时可以用 Companion 代替
     println(MyClass.Companion)
+
+    // 调用扩展的成员
+    MyClass.test()
+    println(MyClass.pet)
 }
